d9f2b5c2b8f2c9e75a8d48740fceeab05d1e5ee6
Leandro Pimentel <leandropimentel2011@gmail.com>
2022-09-26 18:31:32 -0300

    paths name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import { RepositoryList } from "./components/RepositoryList";
+import { RepositoryList } from "../components/RepositoryList";
 
 const Home: NextPage = () => {
   return (
